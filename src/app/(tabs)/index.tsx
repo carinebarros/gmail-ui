120a9341da4fea03111af7599b38dbb501c425bb
Carine <carinebarros@outlook.com>
2024-07-16 16:22:43 -0300

    style: Configs NativeWind

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+const Home = () => (
+  <View className="flex-1 bg-gray-900">
+    <Text>Home asdf</Text>
+  </View>
+);
+
+export default Home;
---
+++ src/components/loading.tsx
@@ -0,0 +1,7 @@
+import { ActivityIndicator } from "react-native";
+
+const Loading = () => (
+  <ActivityIndicator className="flex-1 justify-center items-center bg-gray-900" />
+);
+
+export default Loading;
