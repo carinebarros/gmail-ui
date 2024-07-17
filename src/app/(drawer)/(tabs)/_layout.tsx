9e068bdb289dfd8cc6b020fb9dc2b4a5bfb01365
Carine <carinebarros@outlook.com>
2024-07-17 14:40:47 -0300

    feat: Create and configure Tabs

---
+++ src/app/(drawer)/(tabs)/_layout.tsx
@@ -0,0 +1,71 @@
+import { useMemo } from "react";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+import { colors } from "@/styles/colors";
+import type { IconName } from "@/types";
+
+type RenderTabBarIconParams = {
+  color: string;
+  size: number;
+};
+
+const TabBarIcon =
+  (name: IconName) =>
+  ({ size, color }: RenderTabBarIconParams) =>
+    <MaterialIcons name={name} size={size} color={color} />;
+
+const TabLayout = () => {
+  const tabs = useMemo(
+    () => [
+      {
+        name: "index",
+        options: {
+          tabBarIcon: TabBarIcon("email"),
+        },
+      },
+      {
+        name: "chat",
+        options: {
+          tabBarIcon: TabBarIcon("chat-bubble"),
+        },
+      },
+      {
+        name: "meeting",
+        options: {
+          tabBarIcon: TabBarIcon("videocam"),
+        },
+      },
+    ],
+    []
+  );
+
+  const screenOptions = useMemo(
+    () => ({
+      headerShown: false,
+      tabBarStyle: {
+        backgroundColor: colors.gray[600],
+        borderTopWidth: 0,
+        minHeight: 74,
+      },
+      tabBarItemStyle: {
+        paddingBottom: 34,
+        paddingTop: 14,
+      },
+      tabBarShowLabel: false,
+      tabBarActiveTintColor: colors.orange[500],
+      tabBarInactiveTintColor: colors.gray[400],
+    }),
+    [colors]
+  );
+
+  return (
+    <Tabs screenOptions={screenOptions}>
+      {tabs.map((tab) => (
+        <Tabs.Screen key={tab.name} {...tab} />
+      ))}
+    </Tabs>
+  );
+};
+
+export default TabLayout;
