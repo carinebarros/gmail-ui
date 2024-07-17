73293746d51a0c2664a7691d2ae18c562312d155
Carine <carinebarros@outlook.com>
2024-07-17 08:50:30 -0300

    fix: Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import { fontFamily } from "./src/styles/fontFamily";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all of your component files.
-  content: ["./src/app/**/*.{ts,tsx}"],
+  content: ["./src/app/**/*.{ts,tsx}", "./src/components/**/*.{ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
     extend: { colors, fontFamily },
